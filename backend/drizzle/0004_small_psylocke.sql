5a57b9660726af2d7c0a7df3a6c1848dff4e7166
gustavo <gustavolola.dev@gmail.com>
2026-06-08 14:14:45 -0300

    chore: remover arquivos do drizzle do rastreamento do git

--- backend/drizzle/0004_small_psylocke.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "keywords" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"keyword" text NOT NULL,
-	"source" text DEFAULT 'user' NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX "keywords_keyword_unique" ON "keywords" USING btree ("keyword");
